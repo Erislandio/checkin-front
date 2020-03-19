7361d409102bcde06c8683ca70406e2e20c9aa4d
erislandio.soares <erislandio.soares@acct.global>
2020-03-19 13:28:10 -0300

    ajustes paginas login e create account

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./pages/login/login";
 import SignIn from "./pages/create/signin";
 
 export default function App() {
   return (
-    <Router>
-      <Route path="/" exact component={Login} />
-      <Route path="/signin" exact component={SignIn} />
+    <Router basename={process.env.PUBLIC_URL}>
+      <Switch>
+        <Route path="/" exact component={Login} />
+        <Route path="/signin" exact component={SignIn} />
+      </Switch>
     </Router>
   );
 }
