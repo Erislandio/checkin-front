9a879aa255175d107a48ecd1ffd0982726b61a99
erislandio.soares <erislandio.soares@acct.global>
2020-03-19 13:30:26 -0300

    ajustes paginas login e create account

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Switch, HashRouter } from "react-router-dom";
 import Login from "./pages/login/login";
 import SignIn from "./pages/create/signin";
 
 export default function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route path="/" exact component={Login} />
         <Route path="/signin" exact component={SignIn} />
       </Switch>
-    </Router>
+    </HashRouter>
   );
 }
